f4f17eb4dbfb7d6c41224941266ef89dd4e7ad17
Forrest Bicker <bickerforrest@gmail.com>
2020-05-10 00:00:53 -0400

    add TrackableArray class

--- background.js
+++ background.js
@@ -1,25 +1,48 @@
 function main() {
     var arr = [7, 4, 9, 2, 4, 5, 1]
-    sorts.insertion(arr);
-    console.log(arr);
+    var a = new TrackableArray(arr);
+    console.log(a.arr);
+    console.log(sorts.insertion(arr));
 }
 
-URLSearchParams
+class TrackableArray {
+
+    constructor(array) {
+        this.arr = array;
+        this.n = array.length;
+
+        this.acsesses = 0;
+        this.modifications = 0;
+    }
+
+    get(ix) {
+        this.acsesses++;
+        return this.arr[ix];
+    }
+
+    set(ix, value) {
+        this.modifications++
+        this.arr[ix] = value;
+        this.update();
+    }
+
+    update() {
+
+    }
+}
 
 var sorts = {
     insertion: function (array) {
-        var arr = array;
-        for (var i = 1; i < arr.length; i++) {
-            var key = arr[i];
+        var arr = new TrackableArray(array);
+        for (var i = 1; i < arr.n; i++) {
+            var key = arr.get(i);
             var j = i - 1;
-            console.log(arr + " " + i);
-            while (j >= 0 && key < arr[j]) {
-                arr[j + 1] = arr[j];
+            while (j >= 0 && key < arr.get(j)) {
+                arr.set(j + 1, arr.get(j));
                 j--;
             }
-            arr[j + 1] = key;
+            arr.set(j + 1, key);
         }
-
         return arr;
     }
 }
