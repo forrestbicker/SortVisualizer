de381b6fd6c8170635a36a92f2bb6c3da747f4f5
Forrest Bicker <bickerforrest@gmail.com>
2020-05-15 21:52:39 -0400

    added TrackableArray construtor

--- framework/TrackableArray.ts
+++ framework/TrackableArray.ts
@@ -1,2 +1,11 @@
 class TrackableArray { // todo: make extend Array<number>
+    arr: Array<number>;
+    acsesses: number;
+    modifications: number;
+
+    constructor(array: Array<number>) {
+        this.arr = array;
+        this.acsesses = 0;
+        this.modifications = 0;
+    }
 }
