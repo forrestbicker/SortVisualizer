b2938c8382934396e6c2b2b5da309a6196e2ca0d
Forrest Bicker <bickerforrest@gmail.com>
2020-05-30 22:09:55 -0400

    Create app.tsx

---
+++ src/app/app.tsx
@@ -0,0 +1,5 @@
+import { SortingTask } from "./framework/SortingTask";
+import { Sorter } from "./framework/Sorter";
+import { MergeSort } from "./SortingAlgorithms/MergeSort";
+import { InsertionSort } from "./SortingAlgorithms/InsertionSort";
+import { HeapSort } from "./SortingAlgorithms/HeapSort";
