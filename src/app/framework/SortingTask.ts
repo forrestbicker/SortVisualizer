c5faa88cd42b70408a1282a7bfa48ce4aac4737c
Forrest Bicker <bickerforrest@gmail.com>
2020-06-02 18:40:27 -0400

    added canvas to SortingTask constructor

--- src/app/framework/SortingTask.ts
+++ src/app/framework/SortingTask.ts
@@ -2,14 +2,16 @@ import { TrackableArray } from "./TrackableArray";
 export class SortingTask {
     arr: TrackableArray;
     auxiliaryArrs: Array<TrackableArray>;
-    swaps: number;
-    comparisons: number;
+    canvas: HTMLElement;
+    // swaps: number;
+    // comparisons: number;
 
-    constructor(array: Array<number>) {
-        this.arr = new TrackableArray(array);
+    constructor(array: Array<number>, canvas: HTMLElement) {
+        this.arr = new TrackableArray(array, canvas, false);
         this.auxiliaryArrs = new Array<TrackableArray>();
-        this.swaps = 0;
-        this.comparisons = 0;
+        this.canvas = canvas;
+        // this.swaps = 0;
+        // this.comparisons = 0;
     }
 
     generateNewAuxID(): number {
