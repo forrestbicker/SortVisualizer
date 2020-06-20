697cf2905dc970ac8d903f2849525f0ab610786a
Forrest Bicker <bickerforrest@gmail.com>
2020-06-19 22:49:29 -0400

    Create ArrayDrawer.ts

---
+++ src/app/framework/ArrayDrawer.ts
@@ -0,0 +1,4 @@
+import { TrackableArray } from "./TrackableArray";
+
+export class ArrayDrawer { // TODO: each sorting tash should have an array drawer that is passed down and can tell aux from primary on method call
+}
