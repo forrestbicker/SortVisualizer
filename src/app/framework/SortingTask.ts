50ce5850717a0a27077e28c06fedff7d6ed85f32
Forrest Bicker <bickerforrest@gmail.com>
2020-06-30 23:02:24 -0400

    commented out real-time display code

--- src/app/framework/SortingTask.ts
+++ src/app/framework/SortingTask.ts
@@ -34,4 +34,40 @@ export class SortingTask {
     updatePositions(arr: Array<number>): void { };
             this.tArr.arr[cutoff] = this.tArr.arr[randIx];
             this.tArr.arr[randIx] = temp;
+    // updateCanvas(): void {
+    //     // this.canvas.innerHTML = "";
+    //     // this.updatePositions();
+    //     // this.updateCounters();
+    //     this.canvas.innerHTML += ".\n"
+    // }
+
+    // updateCounters(): void {
+
+    //     let auxAcsesses: number = 0;
+    //     let auxModifications: number = 0;
+
+    //     for (const arr of this.auxiliaryArrs) {
+    //         auxAcsesses += arr.acsesses;
+    //         auxModifications += arr.modifications;
+    //     }
+
+    //     this.canvas.innerHTML += `<text x="20" y="35">
+    //     Acsesses     : ${this.tArr.acsesses}
+    //     Modifications: ${this.tArr.modifications}
+    //     Aux Acs's    : ${auxAcsesses}
+    //     Aux Mod's    : ${auxModifications}  
+    //     </text>`
+    // };
+
+    // updatePositions(): void {
+    //     for (var i = 0; i < this.tArr.arr.length; i++) {
+    //         let height: number = this.tArr.arr[i] * this.cHeightUnit;
+    //         this.canvas.innerHTML += `<rect
+    //         width="${this.cWidthUnit}"
+    //         height="${height}"
+    //         x="${i * this.cWidthUnit}"
+    //         y="${Number(this.canvas.getAttribute("height")) - height}"></rect>`;
+    //     }
+
+    // };
 }
