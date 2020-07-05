a0a638bbe8afa77fd78ee22e1f1717d0c32ce306
Forrest Bicker <bickerforrest@gmail.com>
2020-07-05 19:00:19 -0400

    implemented ASorter method to verify sort accuracy

--- src/framework/ASorter.ts
+++ src/framework/ASorter.ts
@@ -12,11 +12,24 @@ export abstract class ASorter {
     execute(): void {
         this.task.randomize();
         this.sort();
+        this.verify();
         this.task.drawer.display(15);
     }
 
-    verify(): number {
-        return 0 // for sorted, 1 for not sorted
+    verify(): boolean {
+        let prev: number = this.task.tArr.arr[0];
+        this.task.drawer.pushReaderUpdate(0, false)
+        this.task.drawer.pushColorUpdate(0, "#00FF00")
+        for (var i = 1; i < this.task.tArr.getLength(); i++) {
+            let val: number = this.task.tArr.arr[i];
+            this.task.drawer.pushReaderUpdate(i, false) // display effect of moving head without modifying counters
+            if (prev < val) {
+                this.task.drawer.pushColorUpdate(i, "#00FF00")
+            } else {
+                return false
+            }
+        }
+        return true
     }
 }
 
