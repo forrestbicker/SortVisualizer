f5531a57a53c02f843ce6d796c2926ca4896c600
Forrest Bicker <bickerforrest@gmail.com>
2020-11-08 21:17:17 -0500

    added more colors to config

--- src/Utility/Config.ts
+++ src/Utility/Config.ts
@@ -3,5 +3,12 @@ export class Config {
 
     static colors: any = {
         barColor: Config.white, // default color for array element repersentations
+        barSwapHighlight: "#FEB2B2", // hightlight color durring element swaps
+        barCheckHighlight: "#9AE6B4", // highlight color for correctly placed element
+        barErrorHighlight: "#FEB2B2", // highlight color for incorrectly placed element
+
+        readerColor: "#90cdf4",
+
+        textColor: Config.white,
     };
 }
